fdd6792c0cb3005b60cfcc96553e1aed3f004c15
Chichi Ugorji <chyjuls@googlemail.com>
2023-11-19 15:18:12 +0000

    This code is the 4th milestone, it defines a class and adds the __init__method, with its attributes

---
+++ milestone_4.py
@@ -0,0 +1,40 @@
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.word = random.choice(self.word_list)
+        self.word_guessed = ['_' for _ in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.list_of_guesses = []
+    
+    def check_guess(self, guess):
+        guess = guess.lower()
+        self.list_of_guesses.append(guess)
+
+        if guess in self.word:
+            print(f'Good guess! {guess} is in the word.')
+            for i in range(len(self.word)):
+                if self.word[i]==guess:
+                    self.word_guessed[i] = guess
+                if '_' not in self.word_guessed:
+                    print(f"Congratulations! You've guessed the word: {''.join(self.word_guessed)}")
+                    return True    
+            self.num_letters -= 1
+        else: 
+            self.num_lives -= 1
+            print(f'Sorry, {guess} is not in the word.')
+            print(f'You have {self.num_lives} lives left.')
+            
+    def ask_for_input(self):
+        while True:
+            guess = guess.lower()
+            guess = input('Guess a letter: ')
+            if len(guess) != 1 or not guess.isalpha():
+                print('Invalid input. Please enter a single alphabetical character.')
+            elif guess in self.list_of_guesses:
+                print('You already guessed that letter!')
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
